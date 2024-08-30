67662895f60c2725b08dd8d84d5fab2d9e7d02dc
Hazghard <100840855+Hazghard@users.noreply.github.com>
2024-08-30 17:37:14 +0200

    Update index.js
    MAJ pour fonctionnement sur mobile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+console.log("Script chargé sur mobile");
+
 // Fonction pour modifier l'attribut 'accept' une fois que l'élément est chargé dans le DOM
 function updateFileInput() {
     // Sélectionnez l'élément d'entrée de fichier
     const fileInput = document.querySelector('input[type="file"]');
+    console.log("fileInput trouvé :", fileInput);
 
     if (fileInput) {
         // Modifie l'attribut 'accept' pour permettre les fichiers d'image et PDF
@@ -12,15 +15,25 @@ function updateFileInput() {
         
         // Assure que l'attribut 'multiple' est présent
         fileInput.setAttribute('multiple', ''); // Ceci permet la sélection de plusieurs fichiers
-        console.log('Attribut "accept" modifié pour permettre la prise de photo');
+        console.log('Attributs "accept", "capture" et "multiple" modifiés pour permettre la prise de photo');
         
         // Déconnectez l'observateur après modification
         observer.disconnect();
+    } else {
+        console.warn("L'élément d'entrée de fichier n'a pas été trouvé");
     }
 }
 
 // Créez un observateur de mutations pour surveiller les changements dans le DOM
 const observer = new MutationObserver(updateFileInput);
 
-// Configurer l'observateur pour surveiller tout le document pour les changements d'enfants
-observer.observe(document.body, { childList: true, subtree: true });
+// Fonction pour initialiser l'observateur quand le DOM est prêt
+function initObserver() {
+    // Configurer l'observateur pour surveiller tout le document pour les changements d'enfants
+    observer.observe(document.body, { childList: true, subtree: true });
+    console.log("Observateur de mutations initialisé");
+}
+
+// Assure que l'initialisation de l'observateur se produit après le chargement complet du DOM
+document.addEventListener('DOMContentLoaded', initObserver);
+
